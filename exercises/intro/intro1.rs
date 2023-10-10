1f94b8ce9d8f83fca8e6c71189259eabd2e82a4d
H <q>
2023-10-10 20:21:32 +0800

    update

--- exercises/intro/intro1.rs
+++ exercises/intro/intro1.rs
@@ -9,9 +9,10 @@
 // reloaded when you change one of the lines below! Try adding a `println!`
 // line, or try changing what it outputs in your terminal. Try removing a
 // semicolon and see what happens!
-//
+// 
 // Execute `rustlings hint intro1` or use the `hint` watch subcommand for a
 // hint.
+
 
 
 fn main() {
